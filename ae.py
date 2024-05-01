0e6f23b3674abc9e105a29ae1a26cfe0f376a1d7
issaramro <iza04@mail.aub.edu>
2024-05-01 17:19:00 +0300

    ae

---
+++ ae.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+from torchdiffeq import odeint as odeint_torch
+import numpy as np
+from functions import jacobian
+
+### Autoencoder to be used for these cases: ###
+# Supervised case: Supervised = True, x_true is not None, and all ls = 0
+# Known system (Rossler case): RosslerSystem = True, l1 = 1e-1, l2 = 1e-1, l_rossler = 1e-2, l4 = 1e-2, (a,b,c) get updated
+# Unknown system - Autoencoder then SINDy: l1 = 1, l2 = 1
+# Unknown system - SINDy Autoencoder: l1 = 1, l2 = 5*1e-3, l3 = 1e-6, l4 = 1e-6, l5 = 1, l6 = ?, coefficents and theta get updated
+class Autoencoder(nn.Module):
+    def __init__(self, tau, x_true = None, RosslerSystem = False, Supervised = False, input_dim = 3, 
+                 latent_dim = 3, library_dim = 10, state_dim = 3, poly_order = 2,
+                 l={'l1': 1, 'l2': 5*1e-3, 'l3': 1e-6, 'l4': 1e-6, 'l5': 1, 'l6': 0, 'l1_rossler': 0, 'l2_rossler':0}):
+        super(Autoencoder, self).__init__()
+
+        self.tau = tau
+        self.x_true = x_true
+        self.RosslerSystem = RosslerSystem
+        self.Supervised = Supervised
+        self.library_dim = library_dim
+        self.state_dim = state_dim
+        self.poly_order = poly_order
+        self.n_dimensions = state_dim
+        self.degree = poly_order
+        self.l = l
+        if self.RosslerSystem:
+            self.a = nn.Parameter(torch.tensor(1.1,requires_grad=True, dtype = torch.float32))
+            self.b = nn.Parameter(torch.tensor(0.2,requires_grad=True, dtype = torch.float32))
+            self.c = nn.Parameter(torch.tensor(10,requires_grad=True, dtype = torch.float32))
+        else:
+            self.coefficients = nn.Parameter(torch.randn(library_dim, state_dim))
+
+        n = 100
+        self.encoder = nn.Sequential(
+        nn.Linear(input_dim, n),
+        nn.ReLU(),
+        nn.Linear(n, 40),
+        nn.ReLU(),
+        nn.Linear(40, 10),
+        nn.ReLU(),
+        nn.Linear(10, latent_dim) 
+        )
+
+        self.decoder = nn.Sequential(
+        nn.Linear(latent_dim, 10),
+        nn.ReLU(),
+        nn.Linear(10, 40),
+        nn.ReLU(),
+        nn.Linear(40, n),
+        nn.ReLU(),
+        nn.Linear(n, input_dim) 
+        )
+
+    def decode(self, z):
+        decoded = self.decoder(z)
+        return decoded
+    
+    def encode(self, z):
+        encoded = self.encoder(z)
+        return encoded
+    
+    def rossler(self, X):
+        x, y, z = X[:,0], X[:,1], X[:,2]
+        dx = - y - z
+        dy = x + self.a * y
+        dz = self.b - self.c * z + x * z
+        return torch.stack([dx, dy, dz], dim=1)
+    
+    def phi(self, x):
+        library = [torch.ones(x.size(0), 1)]
+        for i in range(self.state_dim):
+            library.append(x[:, i:i+1])  
+
+        if self.poly_order == 2:
+            for i in range(self.state_dim):
+                for j in range(i, self.state_dim):
+                    library.append(x[:, i:i+1] * x[:, j:j+1])
+
+        return torch.cat(library, dim=1)
+    
+    def SINDy_num(self, x):
+        dxdt = torch.matmul(self.phi(x), self.coefficients)
+        return dxdt
+    
+    def integrate(self, x0, t):
+        try:
+            x_pred = odeint_torch(self.SINDy_num, x0, t) 
+        except AssertionError as error:
+            print(f"Caught an error while integrating model: {error}\
+                  \n This might mean that the model is numerically unstable.")
+            return None 
+        return x_pred
+    
+    
+    def loss(self, v, dvdt, criterion):
+
+        with torch.autograd.enable_grad():
+            x = self.encoder(v)
+            v_bar = self.decoder(x)
+
+        loss1 = criterion(v_bar, v)*self.l['l1']
+        loss2 = criterion(x[:, 0], v[:, 0])*self.l['l2']
+        loss = loss1 + loss2
+
+        if self.l['l3'] > 0 or self.l['l4'] > 0:
+
+            dxdt_SINDy = self.SINDy_num(x)
+
+            with torch.autograd.enable_grad():
+                dxdv = jacobian(x, v)
+            dxdt = torch.einsum('ijk,ij->ik', dxdv, dvdt)
+
+            with torch.autograd.enable_grad():
+                dvdx = jacobian(v_bar, x)
+            dvdt_dec = torch.einsum('ijk,ij->ik', dvdx, dxdt_SINDy)
+
+            loss3 = criterion(dxdt, dxdt_SINDy)*self.l['l3']
+            loss4 = criterion(dvdt_dec, dvdt)*self.l['l4']
+
+            loss += (loss3 + loss4)
+
+        if self.l['l5'] > 0:
+            loss5 = torch.norm(self.coefficients, 1)*self.l['l5']
+            loss += loss5
+
+        if self.l['l6'] > 0:
+            x_pred = self.integrate([x[0,0], x[0,1], x[0,2]], torch.tensor(np.linspace(0, (np.pi/100)*len(x), len(x), endpoint=False), requires_grad=False, dtype=torch.float32))
+            if x_pred is not None:
+                x_first = x_pred[:,0]
+                n = len(x_first)
+                x1 = x_first[0: n - 2*self.tau]
+                x2 = x_first[self.tau: n - self.tau]
+                x3 = x_first[2*self.tau: n]
+                x_delayed = [x1, x2, x3]
+                v_delayed = [v[:,0][:n], v[:,1][:n], v[:,2][:n]]
+                loss6 = criterion(x_delayed, v_delayed)*self.l['l6']
+                loss += loss6
+            else:
+                loss += 0
+
+        if self.RosslerSystem:
+            rossler_vals = self.rossler(x)
+            with torch.autograd.enable_grad():
+                dxdv = jacobian(x, v)
+            dxdt = torch.einsum('ijk,ij->ik', dxdv, dvdt)
+            
+            with torch.autograd.enable_grad():
+                dvdx = jacobian(v_bar, x)
+            dvdt_dec = torch.einsum('ijk,ij->ik', dvdx, rossler_vals)
+
+            loss_rossler1 = criterion(rossler_vals, dxdt)*self.l['l1_rossler']
+            loss_rossler2 = criterion(dvdt_dec, dvdt)*self.l['l2_rossler']
+            loss += (loss_rossler1 + loss_rossler2)
+
+        if self.Supervised:
+            loss_supervised = criterion(self.x_true, x)
+            loss += loss_supervised
+
+        return loss
+    
---
+++ functions.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+from torchdiffeq import odeint as odeint_torch
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+
+
+def Rossler(X, t, P):
+    a, b, c = P
+    x, y, z = X
+    return - y - z, x + a*y, b - c*z + x*z
+
+
+def Solution(func, x0, t, P):
+    return odeint(func, x0, t, args = (P,)).T
+
+
+# to be used in MI
+def entropy(p):
+    p_norm = p / np.sum(p)
+    p_norm = p_norm[np.nonzero(p_norm)]
+    S = -np.sum(p_norm* np.log(p_norm))  
+    return S
+
+
+# calculates mutual information between X and Y
+def MI(X,Y,bins):
+    p_XY = np.histogram2d(X,Y,bins)[0]
+    p_X = np.histogram(X,bins)[0]
+    p_Y = np.histogram(Y,bins)[0]
+ 
+    S_X = entropy(p_X)
+    S_Y = entropy(p_Y)
+    S_XY = entropy(p_XY)
+ 
+    MI = S_X + S_Y - S_XY
+    return MI
+
+
+# calculates the percentages of the the False Nearest neighbors for a given range of k values
+def FNN(xs, k_vals, eps, tau):
+    per = []
+    n = len(xs)
+    for k in k_vals:
+        FNN = 0
+        NN = 0
+        for i in range(n - (k * tau) - 1):
+            for j in range(n- (k * tau) - 1):
+                if j != i:
+                    if (eps - np.linalg.norm(xs[i] - xs[j])) > 0:
+                        Rd = 0
+                        for o in range(k):
+                            Rd = Rd + (xs[i + (o - 1) * tau] - xs[j + (o - 1) * tau])**2
+                        D = np.abs(xs[i + k * tau] - xs[j + k * tau])
+                        if Rd != 0 and D/(np.sqrt(Rd)) >= 10:
+                            FNN += 1
+                        else:
+                            NN += 1
+        per.append((FNN/NN)*100)
+    return per
+
+
+
+def jacobian(y, x, create_graph=False, retain_graph=True, allow_unused=False):
+    jac = []
+    for i in range(y.shape[-1]):
+        jac_i = torch.autograd.grad(
+            y[..., i], x, torch.ones_like(y[..., i]), allow_unused=allow_unused, retain_graph=retain_graph, create_graph=create_graph)[0]
+        jac.append(jac_i)
+    return torch.stack(jac, dim=-1)
+
+
+def plot(xs, ys, zs, v1, v2, v3, x_ae, y_ae, z_ae):
+
+    fig, ax = plt.subplots(3, 1, figsize=(8, 9))
+    fig_ = plt.figure(figsize=(13, 13))
+    ax3d = fig_.add_subplot(1, 3, 1, projection='3d')
+
+    ax[0].plot(xs[:len(x_ae)], label='True', color = "k")
+    ax[0].plot(x_ae, label='Transformed',linestyle='dashed', color = "maroon")
+    ax[0].set_xlabel("t")
+    ax[0].set_ylabel("x")
+    ax[0].legend()
+    ax[1].plot(ys[:len(y_ae)], label='True', color = "k")
+    ax[1].plot(y_ae, label='Transformed',linestyle='dashed', color = "maroon")
+    ax[1].set_xlabel("t")
+    ax[1].set_ylabel("y")
+    ax[1].legend()
+    ax[2].plot(zs[:len(z_ae)], label='True', color = 'k')
+    ax[2].plot(z_ae, label='Transformed', linestyle='dashed', color = "maroon")
+    ax[2].set_xlabel("t")
+    ax[2].set_ylabel("z")
+    ax[2].legend()
+    ax3d.plot(x_ae, y_ae, z_ae, lw=.3,label='Transformed', color ='maroon')
+    ax3d.plot(xs, ys, zs, lw=.3,label='True', color= 'k')
+    ax3d.plot(v1, v2, v3, lw=.3, color = 'r', label ="Embedded")
+    ax3d.set_xlabel("x")
+    ax3d.set_ylabel("y")
+    ax3d.set_zlabel("z")
+    ax3d.legend()
+    plt.title("The reconstructed Rossler attractor")
+    plt.tight_layout()
+    plt.show()
+    fig3, ax3 = plt.subplots(1, 3, figsize=(10, 9), subplot_kw={'projection': '3d'})
+    ax3[0].plot(xs, ys, zs, label="True", color = "k")
+    ax3[1].plot(v1, v2, v3, label="Embedded", color = "k")
+    ax3[2].plot(x_ae, y_ae, z_ae, label="Transformed",color = "k")
+    ax3[0].legend()
+    ax3[1].legend()
+    ax3[2].legend()
+    plt.show()
